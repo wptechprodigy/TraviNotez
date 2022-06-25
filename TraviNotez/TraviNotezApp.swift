88f180584966aa12e2a799d885cd27a396076e44
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-06-25 17:53:37 +0100

    Initial Commit

---
+++ TraviNotez/TraviNotezApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraviNotezApp.swift
+//  TraviNotez
+//
+//  Created by waheedCodes on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TraviNotezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
